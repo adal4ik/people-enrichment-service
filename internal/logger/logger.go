ba9851a9c4471582a876892bb408be3eefbfa818
adal4ik <adilet.adilek@gmail.com>
2025-06-22 22:07:59 +0500

    refactor: update NewLogger with customizable config and comments

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -2,6 +2,14 @@ package logger
 
 import "go.uber.org/zap"
 
+// NewLogger initializes a new zap logger with production configuration.
+// It can be customized to set different log levels or formats as needed.
+// Currently, it returns a production logger with default settings.
+// Uncomment the configuration lines to customize the logger further.
 func NewLogger() (*zap.Logger, error) {
+	// cfg := zap.NewProductionConfig()
+	// cfg.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	// return cfg.Build()
+
 	return zap.NewProduction()
 }
