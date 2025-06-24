245195fef795982849714641459ee07f982231f6
adal4ik <adilet.adilek@gmail.com>
2025-06-24 22:22:30 +0500

    feat(db): add down sql for rollback support

---
+++ migrations/001_create_persons_table.down.sql
@@ -0,0 +1,8 @@
+DROP INDEX IF EXISTS idx_persons_created_at;
+DROP INDEX IF EXISTS idx_persons_nationality;
+DROP INDEX IF EXISTS idx_persons_patronymic;
+DROP INDEX IF EXISTS idx_persons_surname;
+DROP INDEX IF EXISTS idx_persons_name;
+
+DROP TABLE IF EXISTS persons;
+DROP TYPE IF EXISTS person_gender;
